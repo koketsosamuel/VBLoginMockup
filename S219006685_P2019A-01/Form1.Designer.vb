9f0b9273592581641e9f618d81e9ef11cac0d12c
Sam <koklathewebguy@users.noreply.github.com>
2019-02-13 10:00:32 +0200

    1

---
+++ S219006685_P2019A-01/Form1.Designer.vb
@@ -0,0 +1,118 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmLogin
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtUsername = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.txtResponse = New System.Windows.Forms.TextBox()
+        Me.llbResponse = New System.Windows.Forms.Label()
+        Me.btnLogin = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtUsername
+        '
+        Me.txtUsername.Location = New System.Drawing.Point(8, 46)
+        Me.txtUsername.Name = "txtUsername"
+        Me.txtUsername.Size = New System.Drawing.Size(200, 20)
+        Me.txtUsername.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(5, 30)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(55, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Username"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(5, 85)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Password"
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(8, 101)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(200, 20)
+        Me.txtPassword.TabIndex = 3
+        '
+        'txtResponse
+        '
+        Me.txtResponse.Location = New System.Drawing.Point(8, 174)
+        Me.txtResponse.Name = "txtResponse"
+        Me.txtResponse.ReadOnly = True
+        Me.txtResponse.Size = New System.Drawing.Size(200, 20)
+        Me.txtResponse.TabIndex = 4
+        '
+        'llbResponse
+        '
+        Me.llbResponse.AutoSize = True
+        Me.llbResponse.Location = New System.Drawing.Point(7, 158)
+        Me.llbResponse.Name = "llbResponse"
+        Me.llbResponse.Size = New System.Drawing.Size(55, 13)
+        Me.llbResponse.TabIndex = 5
+        Me.llbResponse.Text = "Response"
+        '
+        'btnLogin
+        '
+        Me.btnLogin.Location = New System.Drawing.Point(146, 127)
+        Me.btnLogin.Name = "btnLogin"
+        Me.btnLogin.Size = New System.Drawing.Size(62, 26)
+        Me.btnLogin.TabIndex = 6
+        Me.btnLogin.Text = "Login"
+        Me.btnLogin.UseVisualStyleBackColor = True
+        '
+        'frmLogin
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(230, 231)
+        Me.Controls.Add(Me.btnLogin)
+        Me.Controls.Add(Me.llbResponse)
+        Me.Controls.Add(Me.txtResponse)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtUsername)
+        Me.Name = "frmLogin"
+        Me.Text = "Login"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtUsername As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents txtResponse As TextBox
+    Friend WithEvents llbResponse As Label
+    Friend WithEvents btnLogin As Button
+End Class
---
+++ S219006685_P2019A-01/Form1.vb
@@ -0,0 +1,45 @@
+﻿' *********************************************************************************
+' Surname, Initials: Maotomabe, K S
+' Student Number: 219006685
+' Practical: P2019A-01
+' Class name: frmLogin
+' *********************************************************************************
+
+
+'enforcing variable declaration and explicit conversions
+Option Explicit On
+Option Strict On
+
+Public Class frmLogin
+
+    'My variable Declarations
+    Private username As Double
+    Private password As Double
+    Private response As String
+    Private correctPassword As Double
+
+
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+
+        'Assigning text values to their variables and converting to correct data types
+        username = CDbl(txtUsername.Text)
+        password = CDbl(txtPassword.Text)
+        txtResponse.Text = response
+
+        'setting up the correct passsword to be half the username
+        correctPassword = username / 2
+
+        'Checking if the username has a length = 6 and the correct password
+        If Len(username.ToString) = 6 And password = correctPassword Then
+
+            response = "Access Granted"
+
+        Else
+
+            response = "program will be locked down"
+
+        End If
+
+    End Sub
+
+End Class
